48faab12f5dbcd48812af33a975735421a64e73f
irfaneffendi <irfaneffendi8462@gmail.com>
2022-08-31 03:09:50 +0700

    menambah footer

--- index.php
+++ index.php
@@ -336,7 +336,7 @@ error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">Warung Mie Aceh Bang Udin</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
